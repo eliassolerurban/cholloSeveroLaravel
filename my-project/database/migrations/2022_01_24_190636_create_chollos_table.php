bbc497fdb7730af493d4f84ff4bd39c9a8eb033c
Elías <solerurban.elias@gmail.com>
2022-01-24 20:19:11 +0100

    creado el modelo de Chollo

---
+++ my-project/database/migrations/2022_01_24_190636_create_chollos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChollosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chollos', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("titulo");
+            $table->string("descripcion");
+            $table->string("url");
+            $table->string("categoria");
+            $table->integer("puntuacion"); 
+            $table->float("precio");
+            $table->float("precio_descuento");
+            $table->boolean("disponible");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chollos');
+    }
+}
