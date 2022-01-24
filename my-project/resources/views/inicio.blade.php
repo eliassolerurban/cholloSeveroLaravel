404fef783d3c45342b897c920d6d44f4cb848827
Elías <solerurban.elias@gmail.com>
2022-01-24 20:30:46 +0100

    la cosa empieza a funcionar

---
+++ my-project/resources/views/inicio.blade.php
@@ -0,0 +1,4 @@
+@extends('static')
+@section('inicio')
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, aliquid nulla. Ea accusamus odio est. Reprehenderit alias autem quaerat adipisci, tempora, dicta voluptates, error obcaecati quam ratione molestias quasi natus.
+@endsection
