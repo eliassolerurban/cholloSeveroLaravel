67a673003748b51ae862f8ce81357ab5a4ba15ea
Elías <solerurban.elias@gmail.com>
2022-01-25 17:51:52 +0100

    me falta controlar eliminar chollo en inicio

--- my-project/resources/views/inicio.blade.php
+++ my-project/resources/views/inicio.blade.php
@@ -5,7 +5,9 @@
         <div>
             <h4>{{$chollo->titulo}}</h4>
             <a class="btn" href={{ route("formEditar", $chollo->id) }}>Editar</button></a>
-            <a class="btn" href="">Eliminar</a>
+            {{-- para eliminar no tengo que crear vista,
+            sólo una ruta delete que lleve a la función del controlador y un confirm de js--}}
+            <a class="btn" href="">Eliminar</a> 
             <p>Categoría: {{$chollo->categoria}}</p> 
             <p>Descripción: {{$chollo->descripcion}}</p>
             <p>Precio original: {{$chollo->precio}}€</p>
