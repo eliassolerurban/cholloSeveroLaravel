3b9feb0b9908362d0781c8217993e0ebbc394428
Elías <solerurban.elias@gmail.com>
2022-01-31 17:54:42 +0100

    auth

--- my-project/routes/web.php
+++ my-project/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PagesController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get("/", [PagesController::class, 'inicio'])->name('inicio');
@@ -13,3 +14,11 @@ Route::put('editar/{id}', [ PagesController::class, 'editar' ]) -> name('editar'
 Route::get('eliminar/{id}', [ PagesController::class, 'confirmEliminar' ]) -> name('confirmEliminar');
 Route::delete('eliminar/{id}', [ PagesController::class, 'eliminar' ]) -> name('eliminar');
 Route::get('detalle/{id}', [ PagesController::class, 'detalle' ]) -> name('detalle');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
