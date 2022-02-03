be6439eb4d946a00d7f719f3c12d269ddf58ea91
Elías <solerurban.elias@gmail.com>
2022-02-03 15:43:50 +0100

    ahora las rutas son dinámicas

--- my-project/routes/web.php
+++ my-project/routes/web.php
@@ -10,11 +10,11 @@ Route::get("nuevos", [PagesController::class, 'nuevos'])->name('nuevos');
 Route::get("destacados", [PagesController::class, 'destacados'])->name('destacados');
 Route::get("crear", [HomeController::class, 'formCrear'])->name('formCrear');
 Route::post('crear', [ HomeController::class, 'crear' ]) -> name('crear');
-Route::get('editar/{id}', [ HomeController::class, 'formEditar' ]) -> name('formEditar');
-Route::put('editar/{id}', [ HomeController::class, 'editar' ]) -> name('editar');
-Route::get('eliminar/{id}', [ HomeController::class, 'confirmEliminar' ]) -> name('confirmEliminar');
-Route::delete('eliminar/{id}', [ HomeController::class, 'eliminar' ]) -> name('eliminar');
-Route::get('detalle/{id}', [ PagesController::class, 'detalle' ]) -> name('detalle');
+Route::get('editar/{id?}', [ HomeController::class, 'formEditar' ]) -> name('formEditar');
+Route::put('editar/{id?}', [ HomeController::class, 'editar' ]) -> name('editar');
+Route::get('eliminar/{id?}', [ HomeController::class, 'confirmEliminar' ]) -> name('confirmEliminar');
+Route::delete('eliminar/{id?}', [ HomeController::class, 'eliminar' ]) -> name('eliminar');
+Route::get('detalle/{id?}', [ PagesController::class, 'detalle' ]) -> name('detalle');
 
 Auth::routes();
 
