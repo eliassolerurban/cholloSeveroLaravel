e9b5d7b564b87baca44b62a203068466844a41d6
Elías <solerurban.elias@gmail.com>
2022-02-07 20:48:38 +0100

    añadiendo faker y además a medias con migración de N:M

---
+++ my-project/database/factories/CholloFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CholloFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //poner campos de chollos
+        ];
+    }
+}
