853d2bad8b00f68b8b0caab50a88c42b4ba6f092
Elías <solerurban.elias@gmail.com>
2022-02-17 17:40:44 +0100

    solucionado tema editar con checkbox

--- my-project/resources/views/chollos/editar.blade.php
+++ my-project/resources/views/chollos/editar.blade.php
@@ -11,27 +11,22 @@
     @csrf
     <label for="titulo">Título: </label><input type="text" name="titulo" class="form-control mb-2" required autofocus value="{{$chollo->titulo}}">
     <label for="descripcion">Descripción: </label><input type="text" name="descripcion" class="form-control mb-2" required value="{{$chollo->descripcion}}">
-    {{-- //TODO:pasar categorias a checkbox --}}
-    
-    @foreach ($chollo->categoria as $categoriaChollo)
-      {{$categorias}}  
-    {{-- @if($categoriaChollo["id"] == $categorias["id"] )
-      <input name="categorias[]" type="checkbox" value={{ $categoria->id }} checked>{{ $categoria->nombre }} 
-      @else
-      <input name="categorias[]" type="checkbox" value={{ $categoria->id }}>{{ $categoria->nombre }}
-      @endif --}}
-  @endforeach
-  
-
-    {{-- <label for="categorias[]">Categoría: </label>
-    <select name="categorias[]">
-      <option value=""></option>
-    </select>
-    <label for="url">URL:<input type="url" name="url" class="form-control mb-2" required value="{{$chollo->url}}">
+    <div class="form-check">
+      @foreach ($categorias as $categoria)
+        <label for="categorias[]">{{ $categoria->nombre }}</label>
+        <input name="categorias[]" type="checkbox" value={{ $categoria->id }}
+        @foreach ($chollo->categoria as $categoriaChollo)
+          @if($categoriaChollo->pivot->categoria_id == $categoria->id)
+            checked
+          @endif
+        @endforeach
+        >
+      @endforeach
+    </div>
+  <label for="url">URL:<input type="url" name="url" class="form-control mb-2" required value="{{$chollo->url}}">
     <label for="precio">Precio:<input type="number" name="precio" class="form-control mb-2" required value="{{$chollo->precio}}">
     <label for="precio_descuento">Precio actual:<input type="number" name="precio_descuento" class="form-control mb-2" required value="{{$chollo->precio_descuento}}">
     <label for="puntuacion">Puntuación:<input type="number" name="puntuacion" class="form-control mb-2" required value="{{$chollo->puntuacion}}">
-     --}}
     <button class="btn btn-primary btn-block" type="submit">
     Actualizar chollo
     </button>
